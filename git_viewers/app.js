090f8efa3f46c4f952f75b01c465532635933e58
Filip Manole <Filip.Manole@microchip.com>
2019-08-19 17:51:00 +0300

    github-strategy token error

--- git_viewers/app.js
+++ git_viewers/app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
 
 mongoose.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongo/app?authSource=admin`, {useNewUrlParser: true});
 
@@ -11,9 +15,41 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+passport.use(new GitHubStrategy({
+  clientID: "03963de3c3c3f3cc309a",
+  clientSecret: "49309e90a3564ff1e07469db3758cadea2394f3c",
+  callbackURL: "http://localhost:8000/auth/github/callback",
+  profileFields: ['email']
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(accessToken, refreshToken, profile);
+  // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+  //   return cb(err, user);
+  // });
+}
+));
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: ['email']}));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    console.log(req, res);
+    res.redirect('/');
+  });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'adasda',
+  resave: true,
+  saveUninitializes: true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -38,5 +74,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  // User.findById(id, function(err, user) {
+    done(err, {name: "test"});
+  // });
+});
+
 
 module.exports = app;
