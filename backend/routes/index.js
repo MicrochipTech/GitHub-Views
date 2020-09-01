138822f906ea01494e57d2d41a2e1ce77fb079a1
filipmanole <filip.manole@gmail.com>
2020-09-01 12:06:07 +0300

    Removed references to IndexCtrl.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,6 @@ const authRoutes = require("./auth-routes");
 const repoRoutes = require("./repo-routes");
 const userRoutes = require("./user-routes");
 const aggregateChartsRoutes = require("./aggregateCharts-routes");
-const indexCtrl = require("../controllers/IndexCtrl");
 const { updateRepositories } = require("../config/cron-setup");
 const { VERSION } = require("../VERSION");
 
@@ -11,7 +10,6 @@ router.get("/VERSION", (req, res) => {
   res.send(VERSION);
 });
 
-router.get("/", indexCtrl.home);
 router.get("/forceUpdate", async (req, res) => {
   updateRepositories();
   res.send("ok started");
