8f94c91782e1af13600c0b83f458c53024183ed6
filipmanole <filip.manole@gmail.com>
2020-09-03 17:19:04 +0300

    Send email reports when error caught.

--- backend/errors/ErrorHandler.js
+++ backend/errors/ErrorHandler.js
@@ -1,5 +1,32 @@
+var nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: process.env.MAIL_SERVICE,
+  auth: {
+    type: process.env.MAIL_AUTH_TYPE,
+    user: process.env.MAIL_AUTH_USER,
+    clientId: process.env.MAIL_AUTH,
+    clientSecret: process.env.MAIL_AUTH_CLIENT_SECRET,
+    refreshToken: process.env.MAIL_AUTH_REFRESH_TOKEN,
+    accessToken: process.env.MAIL_AUTH_ACCESS_TOKEN,
+    expires: process.env.MAIL_AUTH_EXPIRES,
+  },
+});
+
 function logger(msg, err) {
   console.log(msg);
+
+  const mailOptions = {
+    from: process.env.MAIL_AUTH_USER,
+    to: process.env.MAIL_ADMINS.split(" "),
+    subject: "Report from GitHub Views",
+    html: `<p>${msg}<br><br>${err}</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
 }
 
 module.exports = {
