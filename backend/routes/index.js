e09450c7db55789ac579d2fd17129b68cd6cab88
filipmanole <filip.manole@gmail.com>
2020-09-18 18:15:17 +0300

    Database User Schema update; Script to update database with emails.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,11 @@ const userRoutes = require("./user-routes");
 const aggregateChartsRoutes = require("./aggregateCharts-routes");
 const { updateRepositories } = require("../config/cron-setup");
 const { VERSION } = require("../VERSION");
+const RepositoryModel = require("../models/Repository");
+const UserModel = require("../models/User");
+const GitHubApiCtrl = require("../controllers/GitHubApiCtrl");
+const { logger, errorHandler } = require("../logs/logger");
+const UserCtrl = require("../controllers/UserCtrl");
 
 router.get("/VERSION", (req, res) => {
   res.send(VERSION);
@@ -15,6 +20,59 @@ router.get("/forceUpdate", async (req, res) => {
   res.send("ok started");
 });
 
+router.get("/unset_user_emails", async (req, res) => {
+  try {
+    const users = await UserModel.find({
+      githubId: { $ne: null },
+      token_ref: { $exists: true },
+    }).populate("token_ref");
+
+    const updatePromises = users.map(async (user) => {
+      await UserModel.findOneAndUpdate(
+        { _id: user._id },
+        { $unset: { githubEmails: "" } }
+      );
+    });
+    await Promise.all(updatePromises);
+  } catch (err) {
+    errorHandler(
+      `${arguments.callee.name}: Error caught while getting all emails for users.`,
+      err
+    );
+  }
+
+  res.send("mails unset");
+});
+
+router.get("/get_user_emails", async (req, res) => {
+  try {
+    const users = await UserModel.find({
+      githubId: { $ne: null },
+      token_ref: { $exists: true },
+    }).populate("token_ref");
+
+    let usersToUpdate = users.length;
+
+    const updatePromises = users.map(async (user) => {
+      const success = await UserCtrl.updateProfile(user);
+      if (success) {
+        usersToUpdate -= 1;
+      } else {
+        logger.warn(`Email update fail for user ${user.username}.`);
+      }
+
+      logger.info(`${usersToUpdate} users left to update...`);
+    });
+    await Promise.all(updatePromises);
+  } catch (err) {
+    errorHandler(
+      `${arguments.callee.name}: Error caught while getting emails for users.`,
+      err
+    );
+  }
+
+  res.send("mails stored");
+});
 router.use("/auth", authRoutes);
 router.use("/repo", repoRoutes);
 router.use("/user", userRoutes);
