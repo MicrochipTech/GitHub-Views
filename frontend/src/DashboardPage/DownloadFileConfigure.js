9142d66bf3e157543019ab9d458ca274613fd117
Alexandru Niculae <alex2august@gmail.com>
2020-09-24 11:17:57 +0300

    Added close button to configure download file modal

--- frontend/src/DashboardPage/DownloadFileConfigure.js
+++ frontend/src/DashboardPage/DownloadFileConfigure.js
@@ -7,6 +7,7 @@ import {
   FormControlLabel,
   Button,
 } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 import FilterableRepos from "./FilterableRepos";
 
 const STRATEGY_ALL = 1,
@@ -46,106 +47,122 @@ function DownloadFileConfigure({ open, onDownload, onClose }) {
 
   return (
     <Modal open={open} onClose={onClose}>
-      <div className="bareModal">
-        {step === 0 && (
-          <div>
-            <div className="padding20">
-              <Typography variant="h6">
-                Configure the file to download
-              </Typography>
-              <Typography variant="subtitle1">
-                Select what repos to include
-              </Typography>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={selectStrategy === STRATEGY_ALL}
-                    onChange={(e) =>
-                      setSelectStrategy(
-                        e.target.checked ? STRATEGY_ALL : STRATEGY_NONE
-                      )
-                    }
-                    color="primary"
-                  />
-                }
-                label="All Repos"
-              />
-              <br />
-              <span>
-                or manually select just a part of them (at leaset one)
-              </span>
-            </div>
-            <hr />
-            <div
-              style={{
-                maxHeight: "300px",
-                overflowY: "scroll",
-                padding: "20px",
-              }}
-            >
-              <FilterableRepos
-                allRepos={[...repos.userRepos, ...repos.sharedRepos]}
-                selectedRepos={reposToDownload}
-                onChange={(id, selected) => {
-                  if (selected) {
-                    setReposToDownload([...reposToDownload, id]);
-                  } else {
-                    const newRtd = [...reposToDownload];
-                    newRtd.splice(newRtd.indexOf(id), 1);
-                    setReposToDownload(newRtd);
-                    setSelectStrategy(STRATEGY_CUSTOM);
-                  }
-                }}
-              />
-            </div>
-            <hr />
-            <div className="padding20">
-              <Button
-                disabled={reposToDownload.length === 0}
-                onClick={() => setStep(1)}
+      <div>
+        <div className="bareModal">
+          {step === 0 && (
+            <div>
+              <div
+                className="closeBtnWrapper"
+                style={{ textAlign: "right", padding: "10px 10px 0px 0px" }}
               >
-                Next
-              </Button>
+                <CloseIcon onClick={onClose} style={{ cursor: "pointer" }} />
+              </div>
+              <div className="padding20">
+                <Typography variant="h6">
+                  Configure the file to download
+                </Typography>
+                <Typography variant="subtitle1">
+                  Select what repos to include
+                </Typography>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={selectStrategy === STRATEGY_ALL}
+                      onChange={(e) =>
+                        setSelectStrategy(
+                          e.target.checked ? STRATEGY_ALL : STRATEGY_NONE
+                        )
+                      }
+                      color="primary"
+                    />
+                  }
+                  label="All Repos"
+                />
+                <br />
+                <span>
+                  or manually select just a part of them (at leaset one)
+                </span>
+              </div>
+              <hr />
+              <div
+                style={{
+                  maxHeight: "300px",
+                  overflowY: "scroll",
+                  padding: "20px",
+                }}
+              >
+                <FilterableRepos
+                  allRepos={[...repos.userRepos, ...repos.sharedRepos]}
+                  selectedRepos={reposToDownload}
+                  onChange={(id, selected) => {
+                    if (selected) {
+                      setReposToDownload([...reposToDownload, id]);
+                    } else {
+                      const newRtd = [...reposToDownload];
+                      newRtd.splice(newRtd.indexOf(id), 1);
+                      setReposToDownload(newRtd);
+                      setSelectStrategy(STRATEGY_CUSTOM);
+                    }
+                  }}
+                />
+              </div>
+              <hr />
+              <div className="padding20">
+                <Button
+                  disabled={reposToDownload.length === 0}
+                  onClick={() => setStep(1)}
+                >
+                  Next
+                </Button>
+              </div>
             </div>
-          </div>
-        )}
-        {step === 1 && (
-          <div className="padding20">
-            <Typography variant="h6">Configure downlaoded file</Typography>
-            <Typography variant="subtitle1">
-              Select what data to include for each repo (at leaset one)
-            </Typography>
-            {sheets.map((s, idx) => (
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={s.checked}
-                    onChange={(e) => {
-                      const newSheets = [...sheets];
-                      newSheets[idx].checked = e.target.checked;
-                      setSheets(newSheets);
-                    }}
-                    color="primary"
+          )}
+          {step === 1 && (
+            <div>
+              <div
+                className="closeBtnWrapper"
+                style={{ textAlign: "right", padding: "10px 10px 0px 0px" }}
+              >
+                <CloseIcon onClick={onClose} style={{ cursor: "pointer" }} />
+              </div>
+              <div className="padding20">
+                <Typography variant="h6">Configure downlaoded file</Typography>
+                <Typography variant="subtitle1">
+                  Select what data to include for each repo (at leaset one)
+                </Typography>
+                {sheets.map((s, idx) => (
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={s.checked}
+                        onChange={(e) => {
+                          const newSheets = [...sheets];
+                          newSheets[idx].checked = e.target.checked;
+                          setSheets(newSheets);
+                        }}
+                        color="primary"
+                      />
+                    }
+                    label={s.name}
                   />
-                }
-                label={s.name}
-              />
-            ))}
-            <Button onClick={() => setStep(0)}>Back</Button>
-            <Button
-              disabled={sheets.filter((s) => s.checked).length === 0}
-              onClick={() => {
-                const selectedRepos = [
-                  ...repos.userRepos,
-                  repos.sharedRepos,
-                ].filter((r) => reposToDownload.indexOf(r._id) !== -1);
-                onDownload(selectedRepos, sheets);
-              }}
-            >
-              Download
-            </Button>
-          </div>
-        )}
+                ))}
+                <Button onClick={() => setStep(0)}>Back</Button>
+                <Button
+                  disabled={sheets.filter((s) => s.checked).length === 0}
+                  onClick={() => {
+                    const selectedRepos = [
+                      ...repos.userRepos,
+                      repos.sharedRepos,
+                    ].filter((r) => reposToDownload.indexOf(r._id) !== -1);
+                    onDownload(selectedRepos, sheets);
+                  }}
+                >
+                  Download
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </Modal>
   );
