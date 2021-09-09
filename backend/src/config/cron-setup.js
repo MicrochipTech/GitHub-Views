a8c4eefa7f3cb57c80e489158a70fcd42b4bfc1a
Filip Manole <filip.manole@gmail.com>
2021-09-09 15:35:25 +0300

    Use "await to" instead of try-catch;

--- backend/src/config/cron-setup.js
+++ backend/src/config/cron-setup.js
@@ -8,10 +8,10 @@ const {
   updateRepositoriesGenerator,
   runGenerator,
 } = require("./updateRepositoriesWithBackoff");
+const to = require("await-to-js").default;
 
 /* Using back off is way slower because requests are made sequential.
 Still, being slower actually reduces the chance of making 5000+ requests per hour. */
-const UPDATE_WITH_BACK_OFF_ON_ERROR = false;
 
 const MODES = {
   LEGACY: 0,
@@ -26,9 +26,9 @@ async function setRepoUpdateCron() {
     `${arguments.callee.name}: Setting a cronjob every day, to update repositories.`
   );
   cron.schedule("25 12 * * *", async () => {
-    try {
-      await updateRepositoriesTask();
-    } catch (err) {
+
+    const [err] = await to(updateRepositoriesTask());
+    if (err) {
       errorHandler(
         `${arguments.callee.name}: Error caught in daily repositories update.`,
         err
@@ -41,47 +41,49 @@ async function updateRepositoriesTask() {
 
   switch (UPDATE_MODE) {
     case MODES.LEGACY:
+      
       logger.info("LEGACY MODE");
-      try {
-        await updateRepositoriesAsynch();
-        // WARNING: running cleanDuplicates here is a dirty find
-        // TODO: the root cause of the duplicates creation should be fixed
-        // INFO: this will require in-depts debugging of the updateRepositoriesAsynch function
-        await cleanDuplicates();
-      } catch (err) {
+      /* WARNING: running cleanDuplicates here is a dirty find
+       * TODO: the root cause of the duplicates creation should be fixed
+       * INFO: this will require in-depts debugging of the updateRepositoriesAsynch function */
+
+      const [err] = await to(updateRepositoriesAsynch());
+      !err && ([err] = await to(cleanDuplicates()));
+      if (err) {
         errorHandler(
           `${arguments.callee.name}: Error caught in daily repositories update.`,
           err
         );
       }
-
       break;
+
     case MODES.CURSORS:
+
       logger.info("CURSOR MODE");
-      try {
-        await dailyUpdate();
-      } catch (err) {
+      const [err] = await to(dailyUpdate());
+      if (err) {
         errorHandler(
           `${arguments.callee.name}: Error caught in daily repositories update.`,
           err
         );
       }
-
       break;
+
     case MODES.BACK_OFF:
+
       logger.info("BACK-OFF MODE");
-      try {
-        await runGenerator(updateRepositoriesGenerator());
-      } catch (err) {
+      const [err] = await to(runGenerator(updateRepositoriesGenerator()));
+      if (err) {
         errorHandler(
           `${arguments.callee.name}: Error caught in daily repositories update.`,
           err
         );
       }
-
       break;
+
     default:
-      console.log("Mode not supported...");
+
+    console.log("Mode not supported...");
       return;
   }
 }
